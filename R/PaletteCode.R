a14e9b493ab1786c3a95af028d2cf4484b920e12
Blake Robert Mills <brm2143@columbia.edu>
2021-12-11 09:51:07 -0500

    Initial Push

---
+++ R/PaletteCode.R
@@ -0,0 +1,115 @@
+# List of Color Palettes and the order in which they are printed
+
+
+#' Complete list of palettes
+#'
+#' Use \code{\link{met.brewer}} to construct palettes.
+#'
+#' @export
+MetPalettes <- list(
+  Austria = list(c("#a40000", "#16317d", "#007e2f", "#ffcd12", "#b86092", "#721b3e", "#00b7a7"), c(1, 2, 3, 4, 6, 5, 7)),
+  Cassatt = list(c("#b1615c", "#d88782", "#e3aba7", "#edd7d9", "#c9c9dd", "#9d9dc7", "#8282aa", "#5a5a83"), c(3, 6, 1, 8, 4, 5, 2, 7)),
+  Degas = list(c("#591d06", "#96410e", "#e5a335", "#556219", "#418979", "#2b614e", "#053c29"), c(5, 2, 4, 3, 7, 1, 6)),
+  Egypt = list(c("#dd5129", "#0f7ba2", "#43b284", "#fab255"), c(1, 2, 3, 4)),
+  Gaugin = list(c("#b04948", "#811e18", "#9e4013", "#c88a2c", "#4c6216", "#1a472a"), c(2, 5, 4, 3, 1, 6)),
+  Greek = list(c("#3c0d03", "#8d1c06", "#e67424", "#ed9b49", "#f5c34d"), c(2, 3, 5, 1, 4)),
+  Hokusai = list(c("#6d2f20", "#b75347", "#df7e66", "#e09351", "#edc775", "#94b594", "#224b5e"), c(2, 7, 4, 6, 5, 1, 3)),
+  Ingres= list(c("#041d2c", "#06314e", "#18527e", "#2e77ab", "#d1b252", "#a97f2f", "#7e5522", "#472c0b"), c(4, 5, 3, 6, 2, 7, 1, 8)),
+  Isfahan1 = list(c("#4e3910", "#845d29", "#d8c29d", "#4fb6ca", "#178f92", "#175f5d", "#1d1f54"), c(5, 2, 4, 6, 3, 7, 1)),
+  Isfahan2 = list(c("#d7aca1", "#ddc000", "#79ad41", "#34b6c6", "#4063a3"), c(4, 2, 3, 5, 1)),
+  Juarez = list(c("#a82203", "#208cc0", "#f1af3a", "#cf5e4e", "#637b31", "#003967"), c(5, 2, 3, 4, 6, 1)),
+  Klimt = list(c("#df9ed4", "#c93f55", "#eacc62", "#469d76", "#3c4b99", "#924099"), c(5, 2, 3, 4, 6, 1)),
+  Manet = list(c("#3b2319", "#80521c", "#d29c44", "#ebc174", "#ede2cc", "#7ec5f4", "#4585b7", "#225e92", "#183571", "#312a76", "#5e65be"), c(2, 5, 8, 3, 4, 9, 1, 6, 7)),
+  Monet = list(c("#4e6d58", "#749e89", "#abccbe", "#e3cacf", "#c399a2", "#9f6e71", "#41507b", "#7d87b2", "#c2cae3"), c(2, 5, 8, 3, 4, 9, 1, 6, 7)),
+  Moreau = list(c("#421600", "#792504", "#bc7524", "#8dadca", "#527baa", "#104839", "#082844"), c(2, 5, 3, 4, 7, 1, 6)),
+  Morgenstern = list(c("#7c668c", "#b08ba5", "#dfbbc8", "#ffc680", "#ffb178", "#db8872", "#a56457"), c(6, 5, 4, 7, 3, 2, 1)),
+  Nattier = list(c("#52271c", "#944839", "#c08e39", "#7f793c", "#565c33", "#184948", "#022a2a"), c(1, 6, 5, 2, 3, 4, 7)),
+  NewKingdom = list(c("#e1846c", "#9eb4e0", "#e6bb9e", "#9c6849", "#735852"), c(2, 1, 3, 4, 5)),
+  Pillement = list(c("#a9845b", "#697852", "#738e8e", "#44636f", "#2b4655", "#0f252f"), c(4, 3, 2, 5, 1, 6)),
+  Pissaro = list(c("#134130", "#4c825d", "#8cae9e", "#8dc7dc", "#508ca7", "#1a5270", "#0e2a4d"), c(5, 2, 3, 6, 4, 7, 1)),
+  Redon = list(c("#5b859e", "#1e395f", "#75884b", "#1e5a46", "#df8d71", "#af4f2f", "#d48f90", "#732f30", "#ab84a5", "#59385c", "#d8b847", "#b38711"), c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)),
+  Robert = list(c("#11341a", "#375624", "#6ca4a0", "#487a7c", "#18505f", "#062e3d"), c(3, 2, 4, 5, 1, 6)),
+  Stevens = list(c("#042e4e", "#307d7f", "#598c4c", "#ba5c3f", "#a13213", "#470c00"), c(4, 2, 3, 5, 1, 6)),
+  Tara = list(c("#eab1c6", "#d35e17", "#e18a1f", "#e9b109", "#829d44"), c(1, 3, 2, 5, 4)),
+  Thomas = list(c("#b24422", "#c44d76", "#4457a5", "#13315f", "#b1a1cc", "#59386c", "#447861", "#7caf5c"), c(3, 2, 8, 6, 1, 4, 7, 5)),
+  Tiepolo = list(c("#802417", "#c06636", "#ce9344", "#e8b960", "#646e3b", "#2b5851", "#508ea2", "#17486f"), c(1, 2, 8, 6, 3, 5, 7, 4)),
+  Troy = list(c("#421401", "#6c1d0e", "#8b3a2b", "#c27668", "#7ba0b4", "#44728c", "#235070", "#0a2d46"), c(2, 7, 4, 5, 1, 8, 3, 6)),
+  VanGogh1 = list(c("#2c2d54", "#434475", "#6b6ca3", "#969bc7", "#87bcbd", "#89ab7c", "#6f9954"), c(3, 5, 7, 4, 6, 2, 1)),
+  VanGogh2 = list(c("#bd3106", "#d9700e", "#e9a00e", "#eebe04", "#5b7314", "#c3d6ce", "#89a6bb", "#454b87"), c(1, 5, 8, 2, 7, 4, 6, 3)),
+  Veronese = list(c("#67322e", "#99610a", "#c38f16", "#6e948c", "#2c6b67", "#175449", "#122c43"), c(5, 1, 7, 2, 3, 6, 4)),
+  Wissing= list(c("#4b1d0d", "#7c291e", "#ba7233", "#3a4421", "#2d5380"), c(2, 3, 5, 4, 1))
+)
+
+# Function for generating palettes
+
+#' Met Palette Generator
+#'
+#' These are a handful of color palettes from Met.
+#'
+#' @param name Name of Palette. Choices are:
+#' \code{Austria}, \code{Cassatt}, \code{Degas}, \code{Egypt}, \code{Gaugin}, \code{Greek},
+#' \code{Hokusai}, \code{Ingres}, \code{Isfahan1}, \code{Klimt}, \code{Monet}, \code{Moreau},
+#' \code{Morgenstern}, \code{Nattier}, \code{NewKingdom}, \code{Pillement}, \code{Pissaro}, \code{Redon},
+#' \code{Robert}, \code{Stevens}, \code{Tara}, \code{Thomas}, \code{Tiepolo}, \code{Troy},
+#' \code{VanGogh1}, \code{VanGogh2}, \code{Veronese}, and \code{Wissing}
+#' @param n Number of desired colors. If number of requested colors is beyond the scope of the palette,
+#' colors are automatically interpolated. If n is not provided, the length of the palette is used.
+#' @param type Either "continuous" or "discrete". Use continuous if you want to automatically
+#' interpolate between colors.
+#' @return A vector of colors.
+#' @examples
+#' met.brewer("VanGogh1")
+#' met.brewer("Tiepolo", 6)
+#' met.brewer("Troy", 10, "continuous")
+#' @keywords colors
+#' @export
+met.brewer <- function(name, n, type = c("discrete", "continuous")) {
+
+  palette <- MetPalettes[[name]]
+
+  if (is.null(palette)|is.numeric(name)){
+    stop("Palette does not exist.")
+  }
+
+  if (missing(n)) {
+    n <- length(palette[[1]])
+  }
+
+  if (missing(type)) {
+    if(n > length(palette[[1]])){type <- "continuous"}
+    else{type <- "discrete"}
+  }
+
+  type <- match.arg(type)
+
+
+  if (type == "discrete" && n > length(palette[[1]])) {
+    stop("Number of requested colors greater than what discrete palette can offer, \n  use as continuous instead.")
+  }
+
+  out <- switch(type,
+                continuous = grDevices::colorRampPalette(palette[[1]])(n),
+                discrete = palette[[1]][sort(palette[[2]][1:n])],
+  )
+  structure(out, class = "palette", name = name)
+
+}
+
+# Function for printing palette
+
+#' @export
+#' @importFrom grDevices rgb
+#' @importFrom graphics rect par image text
+
+print.palette <- function(x, ...) {
+  n <- length(x)
+  old <- par(mar = c(0.5, 0.5, 0.5, 0.5))
+  on.exit(par(old))
+
+  image(1:n, 1, as.matrix(1:n), col = x,
+        ylab = "", xaxt = "n", yaxt = "n", bty = "n")
+
+  rect(0, 0.92, n + 1, 1.08, col = rgb(1, 1, 1, 0.8), border = NA)
+  text((n + 1) / 2, 1, labels = attr(x, "name"), cex = 2.5, family = "serif")
+}
+
